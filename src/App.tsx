bf4c81673577802a330d685cd38c20243b08074b
jwjeffers <jwjeffers@users.noreply.github.com>
2026-04-09 22:03:05 -0400

    Fix unused useEffect in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ShipTerminal } from './ShipTerminal';
 import { Lobby } from './Lobby';
 import { BootSequence } from './BootSequence';
